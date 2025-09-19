0394eddf5bd32cbe5624d1bd68ce45c91dbd785e
JeremyN <jeremy_naugle@yahoo.com>
2025-09-19 14:48:13 -0400

    add templates, routes, and basic styles

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "<h1>Hello from Flask</h1>"
+def index():
+    return render_template("index.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/apps")
+def apps():
+    return render_template("apps.html")
 
 if __name__ == "__main__":
-    # Use PORT from env (Render sets this), else default to 5001 locally
     port = int(os.environ.get("PORT", 5001))
     app.run(host="0.0.0.0", port=port)
 
